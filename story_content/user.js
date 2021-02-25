4ff1492c0bf818eba10e40114f14220502581564
elearningva <69156794+rbvirtualsolutions@users.noreply.github.com>
2021-02-25 20:39:07 +0800

    ReviewGameboard_SciQ2

---
+++ story_content/user.js
@@ -0,0 +1,188 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5r6uLuhQtkA":
+        Script1();
+        break;
+      case "6JuVMzg1c2U":
+        Script2();
+        break;
+      case "5mcE4n9nYOe":
+        Script3();
+        break;
+      case "6PV0b1DENeJ":
+        Script4();
+        break;
+      case "6ly7FbsWiIt":
+        Script5();
+        break;
+      case "5me3SuscbnH":
+        Script6();
+        break;
+      case "6lvNMrAaOWd":
+        Script7();
+        break;
+      case "6ZOKDUwBaCX":
+        Script8();
+        break;
+      case "64jzqnw7CZe":
+        Script9();
+        break;
+      case "6j9MmjFgb89":
+        Script10();
+        break;
+      case "5WlFyplDXvl":
+        Script11();
+        break;
+      case "6PJCSRobGnQ":
+        Script12();
+        break;
+      case "5ncvuUHHJaO":
+        Script13();
+        break;
+      case "5dGlZ39MVzQ":
+        Script14();
+        break;
+      case "5ynpDBVffsn":
+        Script15();
+        break;
+  }
+}
+
+function Script1()
+{
+  var player = GetPlayer();
+player.SetVar("GamePiece_1", "False");
+player.SetVar("GamePiece_2", "False");
+player.SetVar("GamePiece_3", "False");
+player.SetVar("Q1_Answered_Correctly", "False");
+player.SetVar("Q1_Answered_Incorrectly", "False");
+player.SetVar("Q2_Answered_Correctly", "False");
+player.SetVar("Q2_Answered_Incorrectly", "False");
+player.SetVar("Q3_Answered_Correctly", "False");
+player.SetVar("Q3_Answered_Incorrectly", "False");
+player.SetVar("Q4_Answered_Correctly", "False");
+player.SetVar("Q4_Answered_Incorrectly", "False");
+player.SetVar("Q5_Answered_Correctly", "False");
+player.SetVar("Q5_Answered_Incorrectly", "False");
+player.SetVar("Q6_Answered_Correctly", "False");
+player.SetVar("Q6_Answered_Incorrectly", "False");
+player.SetVar("Q7_Answered_Correctly", "False");
+player.SetVar("Q7_Answered_Incorrectly", "False");
+player.SetVar("Q8_Answered_Correctly", "False");
+player.SetVar("Q8_Answered_Incorrectly", "False");
+player.SetVar("Q9_Bonus_Answered_Correctly", "False");
+player.SetVar("Q9_Bonus_Answered_Incorrectly", "False");
+player.SetVar("Q10_Answered_Correctly", "False");
+player.SetVar("Q10_Answered_Incorrectly", "False");
+player.SetVar("Q11_Answered_Correctly", "False");
+player.SetVar("Q11_Answered_Incorrectly", "False");
+player.SetVar("Q12_Answered_Correctly", "False");
+player.SetVar("Q12_Answered_Incorrectly", "False");
+player.SetVar("Q13_Answered_Correctly", "False");
+player.SetVar("Q13_Answered_Incorrectly", "False");
+player.SetVar("Q14_Answered_Correctly", "False");
+player.SetVar("Q14_Answered_Incorrectly", "False");
+player.SetVar("Q1_Answered_Correctly", "False");
+player.SetVar("Roll_1_Animation_COMPLETE", "False");
+player.SetVar("Roll_2_Animation_COMPLETE", "False");
+player.SetVar("Roll_3_Animation_COMPLETE", "False");
+player.SetVar("Roll_4_Animation_COMPLETE", "False");
+player.SetVar("Roll_5_Animation_COMPLETE", "False");
+player.SetVar("Roll_6_Animation_COMPLETE", "False");
+player.SetVar("Roll_7_Animation_COMPLETE", "False");
+player.SetVar("Roll_8_Animation_COMPLETE", "False");
+player.SetVar("Roll_9_Animation_COMPLETE", "False");
+player.SetVar("Roll_10_Animation_COMPLETE", "False");
+player.SetVar("Roll_11_Animation_COMPLETE", "False");
+player.SetVar("Roll_12_Animation_COMPLETE", "False");
+player.SetVar("Roll_13_Animation_COMPLETE", "False");
+player.SetVar("Roll_14_Animation_COMPLETE", "False");
+player.SetVar("Score", "0");
+player.SetVar("Space_1_VISITED", "False");
+player.SetVar("Space_2_VISITED", "False");
+player.SetVar("Space_3_VISITED", "False");
+player.SetVar("Space_4_VISITED", "False");
+player.SetVar("Space_5_VISITED", "False");
+player.SetVar("Space_6_VISITED", "False");
+player.SetVar("Space_7_VISITED", "False");
+player.SetVar("Space_8_VISITED", "False");
+player.SetVar("Space_9_Bonus_VISITED", "False");
+player.SetVar("Space_10_VISITED", "False");
+player.SetVar("Space_11_VISITED", "False");
+player.SetVar("Space_12_VISITED", "False");
+player.SetVar("Space_13_VISITED", "False");
+player.SetVar("Space_14_VISITED", "False");
+}
+
+function Script2()
+{
+  document.getElementById('light-box-close').style.display = "none";
+}
+
+function Script3()
+{
+  document.getElementById('light-box-close').style.display = "none";
+}
+
+function Script4()
+{
+  document.getElementById('light-box-close').style.display = "none";
+}
+
+function Script5()
+{
+  document.getElementById('light-box-close').style.display = "none";
+}
+
+function Script6()
+{
+  document.getElementById('light-box-close').style.display = "none";
+}
+
+function Script7()
+{
+  document.getElementById('light-box-close').style.display = "none";
+}
+
+function Script8()
+{
+  document.getElementById('light-box-close').style.display = "none";
+}
+
+function Script9()
+{
+  document.getElementById('light-box-close').style.display = "none";
+}
+
+function Script10()
+{
+  document.getElementById('light-box-close').style.display = "none";
+}
+
+function Script11()
+{
+  document.getElementById('light-box-close').style.display = "none";
+}
+
+function Script12()
+{
+  document.getElementById('light-box-close').style.display = "none";
+}
+
+function Script13()
+{
+  document.getElementById('light-box-close').style.display = "none";
+}
+
+function Script14()
+{
+  document.getElementById('light-box-close').style.display = "none";
+}
+
+function Script15()
+{
+  document.getElementById('light-box-close').style.display = "none";
+}
+
